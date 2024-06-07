ef4e0858a98e7a0d1112a89dadcbe423a3b4a4e2
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-06-07 15:12:37 +0900

    04_02_確認課題

--- main.cpp
+++ main.cpp
@@ -15,12 +15,18 @@ public:
 
 int main()
 {
-	NumQuite<int, float> numQuite1(10, 9.9f);			//int型とfloat型
-	NumQuite<int, double> numQuite2(2, 2.222);			//int型とdouble型
-	NumQuite<float, double> numQuite3(3.4f, 5.678);		//float型とdouble型
+	NumQuite<int, int> numQuite1(10, 3);				//int型とint型
+	NumQuite<int, float> numQuite2(10, 3.5f);			//int型とfloat型
+	NumQuite<int, double> numQuite3(10, 3.567);			//int型とdouble型
+	NumQuite<float, float> numQuite5(3.5f, 2.5f);		//float型とfloat型
+	NumQuite<float, double> numQuite6(3.5f, 2.567);		//float型とdouble型
+	NumQuite<double, double> numQuite4(3.567, 10.123);	//double型とdouble型
 
-	std::cout << "int型とfloat型と比べると、小さい値は：" << numQuite1.Min() << std::endl;
-	std::cout << "int型とdouble型と比べると、小さい値は：" << numQuite2.Min() << std::endl;
-	std::cout << "float型とdouble型と比べると、小さい値は：" << numQuite3.Min() << std::endl;
+	std::cout << "int型とint型と比べると、小さい値は：" << numQuite1.Min() << std::endl;
+	std::cout << "int型とfloat型と比べると、小さい値は：" << numQuite2.Min() << std::endl;
+	std::cout << "int型とdouble型と比べると、小さい値は：" << numQuite3.Min() << std::endl;
+	std::cout << "float型とfloat型と比べると、小さい値は：" << numQuite4.Min() << std::endl;
+	std::cout << "float型とdouble型と比べると、小さい値は：" << numQuite5.Min() << std::endl;
+	std::cout << "double型とdouble型と比べると、小さい値は：" << numQuite6.Min() << std::endl;
 	return 0;
 }
