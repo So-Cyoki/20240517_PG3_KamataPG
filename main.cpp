50f1d7ae0b0721ca35551a4c213b95912fc3d023
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-07-12 14:29:28 +0900

    07_02 確認課題

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
+#include <thread>
+
+void PrintNum(int num) {
+	std::cout << "thread" << num << std::endl;//メッセージを出力
+}
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	std::thread th1(PrintNum, 1);//スレッド1を作成
+	th1.join();//スレッド1の動作完了前に待ち
+
+	std::thread th2(PrintNum, 2);//スレッド2を作成
+	th2.join();//スレッド2の動作完了前に待ち
+
+	std::thread th3(PrintNum, 3);//スレッド3を作成
+	th3.join();//スレッド3の動作完了前に待ち
 
 	return 0;
 }
